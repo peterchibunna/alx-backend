188d80737fb1fdac122b334d294cf11234202e25
Peter Chibunna <peterchibunna@gmail.com>
2023-05-13 18:35:56 +0100

    Task #0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""
+Module: 0. Simple helper function
+"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """Documentation:
+    return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters.
+
+    Page numbers are 1-indexed, i.e. the first page is page 1.
+    """
+    start_index = page_size * (page - 1)
+    end_index = page_size * page
+    return start_index, end_index
