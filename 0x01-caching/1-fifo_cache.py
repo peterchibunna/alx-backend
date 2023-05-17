6b51a2959891ddfdd9caf7f6a9375a1b96cad24f
Peter Chibunna <peterchibunna@gmail.com>
2023-05-17 21:17:01 +0100

    Task #1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -3,9 +3,30 @@
 Module: 1. FIFO Caching
 """
 
-BaseCaching = __import__('base_caching').BaseCaching
+from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
     """Class: FIFO cache implementation
     """
+    def __init__(self, *args, **kwargs):
+        """Initialize super class after initializing child class
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """Implement a put method
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+        if self.cache_data.items().__len__() > BaseCaching.MAX_ITEMS:
+            deleted_key = list(self.cache_data.items())[0][0]
+            del self.cache_data[deleted_key]
+            print('DISCARD: {}'.format(deleted_key))
+
+    def get(self, key):
+        """Implement the get method of class
+        """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
