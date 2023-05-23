5778951d222e3ea946c463d23fa48b36a4aea586
Peter Chibunna <peterchibunna@gmail.com>
2023-05-23 16:29:07 +0100

    Task #0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+0. Basic Flask app
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays title"""
+    try:
+        return render_template("0-index.html")
+    except ValueError:
+        pass
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
